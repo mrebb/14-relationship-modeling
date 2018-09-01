047a99b19c03bb9b2b0355b3e18a10d31eadde36
Mrebb <madhu.rebbana@gmail.com>
2018-09-01 14:06:20 -0700

    server.start fix

--- src/app.js
+++ src/app.js
@@ -25,30 +25,30 @@ app.use(errorHandler);
 // Flag to know if we are up and going
 
 
-// module.exports = {
-//   start: port => app.listen(port, console.log('Listening on PORT', port)),
-//   stop: () => app.close(),
-//   server: app,
-// };
-
-let server;
-
 module.exports = {
-  start: (port) => {
-    if(! server) {
-      server = app.listen(port, (err) => {
-        if(err) { throw err; }
-        console.log(`Server up on ${port}`);
-      });
-    }
-    else {
-      console.log('Server is already running');
-    }
-  },
-  stop: () => {
-    server.close( () => {
-      console.log('Server has been stopped');
-    });
-  },
+  start: port => app.listen(port, console.log('Listening on PORT', port)),
+  stop: () => app.close(),
   server: app,
 };
+
+// let server;
+
+// module.exports = {
+//   start: (port) => {
+//     if(! server) {
+//       server = app.listen(port, (err) => {
+//         if(err) { throw err; }
+//         console.log(`Server up on ${port}`);
+//       });
+//     }
+//     else {
+//       console.log('Server is already running');
+//     }
+//   },
+//   stop: () => {
+//     server.close( () => {
+//       console.log('Server has been stopped');
+//     });
+//   },
+//   server: app,
+// };
